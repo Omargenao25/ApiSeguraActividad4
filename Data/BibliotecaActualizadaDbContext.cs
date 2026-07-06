fbf38d3bbbc96a62b96aba37c55eaf2091db191c
Omargenao25 <omargenao78@gmail.com>
2026-07-05 23:35:43 -0400

    creacion de la carpeta Db context y inyeccion en el program

---
+++ Data/BibliotecaActualizadaDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ApiSeguraActividad4.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiSeguraActividad4.Data
+{
+    public class BibliotecaActualizadaDbContext : DbContext
+    {
+        public BibliotecaActualizadaDbContext(DbContextOptions<BibliotecaActualizadaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Libro>()
+              .HasOne(l => l.Autor)
+              .WithMany(a => a.Libros)
+              .HasForeignKey(l => l.AutorId)
+              .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Libro>()
+              .Property(l => l.Precio)
+              .HasPrecision(18, 2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
+using ApiSeguraActividad4.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<BibliotecaActualizadaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
